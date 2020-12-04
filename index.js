4eb93085f2d6d05c0cea691630b48e17fdc50159
aakash015 <67652208+aakash015@users.noreply.github.com>
2020-12-04 11:13:48 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+
+document.addEventListener('DOMContentLoaded',(e)=>{ 
+    setTimeout(()=>{
+        document.querySelector('.splash').classList.add('display-none');
+        document.querySelector('.navbar').classList.add('fixed-top'); 
+    },5000);
+})
+
